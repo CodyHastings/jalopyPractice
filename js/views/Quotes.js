9dc821bca1af77ee87e4a10fe96109eac55860dd
Cody Hastings <cody@codeup.com>
2022-07-18 13:39:21 -0500

    created Quotes.js view, added template code

---
+++ js/views/Quotes.js
@@ -0,0 +1,23 @@
+export default function QuotesView(props) {
+    return `
+<div class="container">
+    <h1>Dog Facts</h1>
+    <div class="card">
+        <div class="card-body">
+            <p class="dog-fact" style="visibility: hidden">Humans generally see better than dogs, but dogs see much better when there is low-light.</p>
+        </div>
+    </div>
+    <button class="form-control" id="show-fact-btn">Show Fact</button>
+</div>
+`;
+}
+
+export function QuotesEvents() {
+    const btn = document.querySelector("#show-fact-btn");
+    btn.addEventListener("click", function(event) {
+        const facts = document.querySelectorAll(".dog-fact");
+        for (let i = 0; i < facts.length; i++) {
+            facts[i].style.visibility = "";
+        }
+    });
+}
