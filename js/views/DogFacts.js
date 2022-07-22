ed8ddd3965c081de507d60f08a462085a6701f0d
Cody Hastings <cody@codeup.com>
2022-07-22 10:25:06 -0500

    added api key for quotes and dog facts

--- js/views/DogFacts.js
+++ js/views/DogFacts.js
@@ -1,15 +1,38 @@
-export default function DogFactsView(props) {
+// highest level of detail: the HTML function
+// focuses on the HTML for the overall screen
+export default function dogFactsHTMLFunction(props) {
     return `
 <div class="container">
     <h1>Dog Facts</h1>
-    <div class="card">
-        <div class="card-body">
-            <p class="dog-fact" style="visibility: hidden">Humans generally see better than dogs, but dogs see much better when there is low-light.</p>
-        </div>
-    </div>
-    <button class="form-control" id="show-fact-btn">Show Fact</button>
+    
+    ${makeDogFactCards(props.dogFacts)}
+    
+    <button class="form-control" id="show-fact-btn">Show Facts</button>
 </div>
 `;
+}
+
+// next lower level of detail: the function for all of the card HTML
+// uses a loop to iterate over the array and concats each card as it is made
+// by the lowest level function
+function makeDogFactCards(dogFacts) {
+    let html = "";
+    dogFacts.forEach(function(dogFact) {
+        html += makeDogFactCard(dogFact);
+    });
+    return html;
+}
+
+// the lowest level function: assembles the HTML for a single dog fact card
+// using the dog fact that is passed in as a parameter
+function makeDogFactCard(dogFact) {
+    return `
+<div class="card">
+    <div class="card-body">
+        <p class="dog-fact" style="visibility: hidden">${dogFact}</p>
+    </div>
+</div>
+        `;
 }
 
 export function DogFactsEvents() {
