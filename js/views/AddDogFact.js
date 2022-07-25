1ac68d6ae62c434968f5e5b0ec81c0ee1073dc40
Cody Hastings <cody@codeup.com>
2022-07-25 09:42:21 -0500

    added link to insert dog fact to addDogFact. Implmented functionality for adding a dog fact to AddDogFact.js

--- js/views/AddDogFact.js
+++ js/views/AddDogFact.js
@@ -14,4 +14,25 @@ export default function InsertDogView(props) {
 }
 
 export function InsertDogFactEvents() {
+    const insertBtn = document.querySelector("#insert-btn");
+    insertBtn.addEventListener("click", function(event) {
+        const factText = document.querySelector("#dogFactText").value;
+        const requestOptions = {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': DOG_QUOTE_API_KEY
+            },
+            body: JSON.stringify([factText])
+        }
+        fetch("https://dogfacts.fulgentcorp.com:12250/v1/facts", requestOptions)
+            .then(function(response) {
+                if(!response.ok) {
+                    console.log("add dog fact error: " + response.status);
+                } else {
+                    console.log("add dog fact ok");
+                    createView("/dogs");
+                }
+            });
+    })
 }
